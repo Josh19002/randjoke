a86f2e10c94ad27f42f39c18cbcec0d3faac3e9c
Josh19002 <josh123478@gmail.com>
2022-05-26 22:48:59 -0600

    remove console commands, add semicolons

--- javascript/jokes.js
+++ javascript/jokes.js
@@ -1,40 +1,37 @@
-"use strict"
+"use strict";
 async function getJoke1(){
-    document.getElementById("jokegen").disabled = true
-    let response = await fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,sexist,explicit&type=single")
-    let data = await response.json()
-    console.log(data)
-    document.getElementById("joke").textContent = ""
-    document.getElementById("joke").textContent = data.joke
-    document.getElementById("jokegen").disabled = false
-}
+    document.getElementById("jokegen").disabled = true;
+    let response = await fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,sexist,explicit&type=single");
+    let data = await response.json();
+    document.getElementById("joke").textContent = "";
+    document.getElementById("joke").textContent = data.joke;
+    document.getElementById("jokegen").disabled = false;
+};
 async function getJokeMulti(number){
-    document.getElementById("jokegen").disabled = true
-    let response = await fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,sexist,explicit&type=single&amount="+number)
-    let data = await response.json()
-    console.log(data)
-    document.getElementById("joke").textContent = ""
-    let jokenumber = 1
+    document.getElementById("jokegen").disabled = true;
+    let response = await fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,sexist,explicit&type=single&amount="+number);
+    let data = await response.json();
+    document.getElementById("joke").textContent = "";
+    let jokenumber = 1;
     data.jokes.forEach(dataArray => {
-        let div = document.createElement("div")
-        let paragraph = document.createElement("p")
-        let h3 = document.createElement("h3")
-        h3.textContent = `Joke #${jokenumber}`
-        paragraph.textContent = dataArray.joke
-        div.appendChild(h3)
-        div.appendChild(paragraph)
-        jokenumber++
-        document.getElementById("joke").appendChild(div)
+        let div = document.createElement("div");
+        let paragraph = document.createElement("p");
+        let h3 = document.createElement("h3");
+        h3.textContent = `Joke #${jokenumber}`;
+        paragraph.textContent = dataArray.joke;
+        div.appendChild(h3);
+        div.appendChild(paragraph);
+        jokenumber++;
+        document.getElementById("joke").appendChild(div);
     });
-    document.getElementById("jokegen").disabled = false
-}
+    document.getElementById("jokegen").disabled = false;
+};
 function getJoke(){
-    let number = document.getElementById("jokenum").value
-    console.log(number)
+    let number = document.getElementById("jokenum").value;
     if (number == 1) {
-        getJoke1()
+        getJoke1();
     }
     else{
-        getJokeMulti(number)
-    }
-}
+        getJokeMulti(number);
+    };
+};
